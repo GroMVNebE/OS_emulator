44f5718a07dfd4c9ef9de676565ede6390b8c9cc
GroM <egor.maksimovich.s@gmail.com>
2025-09-26 15:30:42 +0300

    Добавлены команды cd и ls, текст в консоли теперь можно выводить в разных цветах, добавлена поддержка переменных окружения (пока только $HOME), доработана обработка ошибок 	modified:   os_emulator.py

--- os_emulator.py
+++ os_emulator.py
@@ -3,6 +3,7 @@ from tkinter import scrolledtext
 from datetime import datetime
 import getpass
 import platform
+import os
 
 
 SPLIT_BLACKLIST = ['echo']
@@ -15,7 +16,9 @@ class ConsoleEmulator:
         # Получаем имя пользователя и название компьютера
         username = getpass.getuser()
         device_name = platform.node()
-        self.root.title(f"Эмулятор - [{username}@{device_name}]")
+        current_dir = os.getcwd()
+        full_path = os.path.join(current_dir)
+        self.root.title(f"Эмулятор - [{username}@{device_name}]     {full_path}")
 
         # Настраиваем поле вывода
         # В нём будет отображаться история команд
@@ -28,13 +31,18 @@ class ConsoleEmulator:
             font=('Courier New', 12)
         )
         self.output_area.pack(expand=True, fill='both')
+        self.output_area.tag_configure("directory", foreground="#87CEEB")
+        self.output_area.tag_configure("executable", foreground="#00FF00")
+        self.output_area.tag_configure("symlink", foreground="#00FFFF")
+        self.output_area.tag_configure("normal", foreground="#FFFFFF")
+        self.output_area.tag_configure("error", foreground="#FF0000")
         self.output_area.config(state='disabled')
 
         # Настраиваем панель ввода
         self.input_frame = tk.Frame(self.root)
         self.input_frame.pack(fill='x', padx=5, pady=5)
         # Настраиваем заголовок для поля ввода
-        self.prompt = tk.Label(self.input_frame, text="$ ", fg='green')
+        self.prompt = tk.Label(self.input_frame, text="$", fg='green')
         self.prompt.pack(side='left')
         # Настраиваем поле ввода
         self.input_field = tk.Entry(self.input_frame, bg='black', fg='white')
@@ -51,15 +59,16 @@ class ConsoleEmulator:
 
         # Выводим команду пользователя в консоль, указывая время отправки для удобства
         time = datetime.now()
-        self.append_text(
-            f"[{time.hour:02}:{time.minute:02}:{time.second:02}]: {command}\n")
-
+        self.append_text(f"[{time.hour:02}:{time.minute:02}:{time.second:02}]: {command}\n")
+        
+        # Обрабатываем команду
         parse_command(self, command)
-
-    def append_text(self, text):
+        
+    
+    def append_text(self, text, tag='normal'):
         # Включаем поле вывода и добавляем в конец переданный текст
         self.output_area.config(state='normal')
-        self.output_area.insert(tk.END, text)
+        self.output_area.insert(tk.END, text, tag)
         # Переводим положение в конец (чтобы добавленный текст был виден)
         self.output_area.see(tk.END)
         # Отключаем поле вывода
@@ -101,6 +110,40 @@ def parse_command(console: ConsoleEmulator, raw_command: str):
     process_command(console, command, args)
 
 
+def parse_env_variable(console: ConsoleEmulator, inp: str | list):
+    if type(inp) == str:
+        while '$' in inp:
+            st = inp.find('$')
+            end = inp.find(' ', inp.find('$'))
+            if end == -1:
+                end = len(inp)
+            var = inp[st:end]
+            if var == "$HOME":
+                inp = inp.replace(var, 'C:/', 1)
+            else:
+                console.append_text(f"Переменная '{var}' не распознана\n", 'error')
+                return None
+        return inp
+    else:
+        idx = 0
+        while idx < len(inp):
+            part = inp[idx]
+            while '$' in part:
+                st = part.find('$')
+                end = part.find(' ', part.find('$'))
+                if end == -1:
+                    end = len(part)
+                var = part[st:end]
+                if var == "$HOME":
+                    part = part.replace(var, 'C:/', 1)
+                else:
+                    console.append_text(f"Переменная '{var}' не распознана\n", 'error')
+                    return None
+            inp[idx] = part
+            idx += 1
+        return inp
+
+
 def process_command(console: ConsoleEmulator, command: str, args: list | None):
     """
     #### Описание:
@@ -116,12 +159,52 @@ def process_command(console: ConsoleEmulator, command: str, args: list | None):
     args – список аргументов
     """
 
+    if args != None:
+        args = parse_env_variable(console, args)
+        if args == None:
+            return
     if command == 'clear':
         console.clear_console()
     elif command == 'echo':
         console.append_text(args + '\n')
+    elif command == 'ls':
+        try:
+            current_dir = os.getcwd()
+            items = os.listdir(current_dir)
+            items.sort()
+            
+            for item in items:
+                full_path = os.path.join(current_dir, item)
+                
+                if os.path.isdir(full_path):
+                    console.append_text(item + '\n', 'directory')
+                elif os.path.islink(full_path):
+                    console.append_text(item + '\n', 'symlink')
+                elif os.access(full_path, os.X_OK):
+                    console.append_text(item + '\n', 'executable')
+                else:
+                    console.append_text(item + '\n', 'normal')
+                    
+        except Exception as e:
+            console.append_text(f"Произошла ошибка: {e}", 'error')
+    elif command == 'cd':
+        if args == []:
+            console.append_text(f"cd требуется указать пункт", 'error')
+            return
+        try:
+            os.chdir(args[0])
+            username = getpass.getuser()
+            device_name = platform.node()
+            current_dir = os.getcwd()
+            full_path = os.path.join(current_dir)
+            console.root.title(f"Эмулятор - [{username}@{device_name}]     {full_path}")
+                    
+        except Exception as e:
+            console.append_text(f"Произошла ошибка: {e}", 'error')
+    elif command == 'exit':
+        exit()
     else:
-        console.append_text(f"Команда '{command}' не распознана\n")
+        console.append_text(f"Команда '{command}' не распознана\n", 'error')
 
     # Запускаем окно Эмулятора консоли, если программа запущена напрямую (не в тестах)
 if __name__ == "__main__":
